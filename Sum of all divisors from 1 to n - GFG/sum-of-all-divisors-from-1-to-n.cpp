cd24f88a4c48c4e09c935bb39f7438660acc9d16
JashandeepSidhu712 <117754690+JashandeepSidhu712@users.noreply.github.com>
2023-07-17 07:55:49 +0530

    Added solution - LeetHub

---
+++ Sum of all divisors from 1 to n - GFG/sum-of-all-divisors-from-1-to-n.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    long long sumOfDivisors(int n)
+    {
+        // Write Your Code here
+        long long sum=0;
+        
+        for(int j=1;j<=n;j++)
+        {
+            sum += j*(n/j);
+        }
+        
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        long long ans  = ob.sumOfDivisors(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
